620f8cd0dfb54e59f8562919aecd6d6acdc64e38
skyrain22 <skyklouis@gmail.com>
2025-07-15 11:41:00 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+// requeire("express")
+import express from "express";
+
+const app = express();
+
+app.listen(8080, () => {
+    console.log("server running at");
+    console.log("Asd111");
+    console.log("we");
+});
